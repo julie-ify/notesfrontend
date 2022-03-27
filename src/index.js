3b6875f38d956a4e9b8024742c301e6bc9d8d76e
Juliana Ifionu <julieifionu@gmail.com>
2022-03-27 23:00:49 +0100

    Set up router and provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { AppState } from './redux/appState';
+import './styles/index.css';
+import App from './components/App';
+import store from './redux/configureStore';
+import { Provider } from 'react-redux'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <AppState>
+        <Router>
+          <App />
+        </Router>
+      </AppState>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +23,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
