72f5f03ad759c1a2ce55e0611a7c4a766bb51b12
Juliana Ifionu <julieifionu@gmail.com>
2022-04-10 10:39:11 +0100

    Create app state using use context

--- src/redux/appState.jsx
+++ src/redux/appState.jsx
@@ -1,36 +1,37 @@
-import React, { useReducer } from "react";
+import React, { useReducer } from 'react';
 
 const initialState = {
-  url: "http://julienotesbackend.herokuapp.com",
-  token: null,
-  username: null
-}
+	url: 'http://julienotesbackend.herokuapp.com',
+	token: null,
+	username: null,
+};
 
-const reducer = ( state, action ) => {
-  let newState;
-  switch(action.type) {
-    case "auth":
-      newState = { ...state, ...action.payload };
-      return newState;
-    case "logout":
-      newState = { ...state, token: null, username: null };
-      return newState;
-    default:
-      return state;
-  }
-}
+const reducer = (state, action) => {
+	let newState;
+	switch (action.type) {
+		case 'auth':
+			newState = { ...state, ...action.payload };
+			return newState;
+		case 'logout':
+			newState = { ...state, token: null, username: null };
+			return newState;
+		default:
+			return state;
+	}
+};
 
 const AppContext = React.createContext(null);
 
 export const AppState = (props) => {
-  const [state, dispatch] = useReducer(reducer, initialState)
-  return <AppContext.Provider value={{state, dispatch}}>
-    {props.children}
-  </AppContext.Provider>
-}
+	const [state, dispatch] = useReducer(reducer, initialState);
+	return (
+		<AppContext.Provider value={{ state, dispatch }}>
+			{props.children}
+		</AppContext.Provider>
+	);
+};
 
 // useAppState hook
 export const useAppState = () => {
-  return React.useContext(AppContext) 
-}
-
+	return React.useContext(AppContext);
+};
