ae3a3cf2c756fcc0876112ce4901de82a9e05fdb
Juliana Ifionu <julieifionu@gmail.com>
2022-04-10 13:22:37 +0100

    Fix http error

--- src/redux/appState.jsx
+++ src/redux/appState.jsx
@@ -1,7 +1,7 @@
 import React, { useReducer } from 'react';
 
 const initialState = {
-	url: 'http://julienotesbackend.herokuapp.com',
+	url: 'https://julienotesbackend.herokuapp.com',
 	token: null,
 	username: null,
 };
