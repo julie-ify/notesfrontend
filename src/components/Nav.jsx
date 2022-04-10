5c59e811fb4f2cd16abecbc569d59dc3372359db
Juliana Ifionu <julieifionu@gmail.com>
2022-04-10 10:39:27 +0100

    Add navigation links to app

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,29 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useAppState } from '../redux/appState';
 import { useNavigate } from 'react-router';
+import { useAppState } from '../redux/appState';
+import Button from '@material-ui/core/Button';
 
-const Nav = (props) => {
-  const navigate = useNavigate();
-  
-  const {state, dispatch } = useAppState();
-  const handleChange = () => {
-    dispatch({type: "logout"})
-    localStorage.removeItem("auth");
-    navigate("/")
-  }
-  return <header>
-    <h1>Juliana's Note Taking App</h1>
-    <p>{localStorage.getItem("auth") ? `Logged in user: ${JSON.parse(localStorage.getItem("auth")).username}`
- : null }</p>
-    <nav>
-      { state.token ? <button onClick={handleChange}>logout</button> : <>
-        {/* <Link to='/'><div>Home</div></Link> */}
-        <Link to='/auth/signup'><div>Signup</div></Link>
-        <Link to='/auth/login'><div>Login</div></Link>
-      </> }
-    </nav>
-  </header>
-}
+const Nav = () => {
+	const navigate = useNavigate();
+
+	const { state, dispatch } = useAppState();
+	const handleChange = () => {
+		dispatch({ type: 'logout' });
+		localStorage.removeItem('auth');
+		navigate('/');
+	};
+	return (
+		<header>
+			<h1>Juliana's Note Taking App</h1>
+			<p>
+				{localStorage.getItem('auth')
+					? `Logged in user: ${
+							JSON.parse(localStorage.getItem('auth')).username
+					  }`
+					: null}
+			</p>
+			<nav>
+				{state.token ? (
+					<Button variant="contained" color="secondary" onClick={handleChange}>
+						Logout
+					</Button>
+				) : (
+					<>
+						<Link to="/auth/signup">
+							<div>Signup</div>
+						</Link>
+						<Link to="/auth/login">
+							<div>Login</div>
+						</Link>
+					</>
+				)}
+			</nav>
+		</header>
+	);
+};
 
 export default Nav;
