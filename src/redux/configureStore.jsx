14841f1a76ff795bbd317acd2770a05e7bba57e1
Juliana Ifionu <julieifionu@gmail.com>
2022-04-10 10:39:42 +0100

    Configure redux store

--- src/redux/configureStore.jsx
+++ src/redux/configureStore.jsx
@@ -1,7 +1,7 @@
 import { applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-import rootReducer from './reducer/notes'
+import rootReducer from './reducer/notes';
 
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
